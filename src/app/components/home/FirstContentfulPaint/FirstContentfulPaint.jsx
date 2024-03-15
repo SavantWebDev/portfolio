6fa824404a60df39f94164b6a158a7ee28b8cf7a
Hudson <hudsonvini26@gmail.com>
2024-03-15 03:15:09 -0300

    Add Glow

--- src/app/components/home/FirstContentfulPaint/FirstContentfulPaint.jsx
+++ src/app/components/home/FirstContentfulPaint/FirstContentfulPaint.jsx
@@ -45,7 +45,35 @@ export default function FirstContentfulPaint() {
             {/* Image Area */}
             <div className={style.imageArea}>
                 <img src="./images/imageFirstContentfulPaint.png" alt="" />
+                <svg className={style.glowBlue} width="500" height="500" viewBox="0 0 888 799" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g filter="url(#filter0_f_579_115)">
+                    <circle cx="443.953" cy="355.747" r="143.237" fill="#084AF3" fill-opacity="0.3"/>
+                    </g>
+                    <defs>
+                    <filter id="filter0_f_579_115" x="0.715332" y="-87.4909" width="886.475" height="886.475" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                    <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_579_115"/>
+                    </filter>
+                    </defs>
+                </svg>
+
+                <span className={style.decoratorLineBall}></span>
+
+                <svg className={style.glowCian} width="350" height="350" viewBox="0 0 563 563" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <g filter="url(#filter0_f_579_112)">
+                    <circle cx="281.5" cy="281.5" r="81.5" fill="#1BCFBD" fill-opacity="0.5"/>
+                    </g>
+                    <defs>
+                    <filter id="filter0_f_579_112" x="0" y="0" width="563" height="563" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                    <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_579_112"/>
+                    </filter>
+                    </defs>
+                </svg>
             </div>
+
         </section>
     )
 }
