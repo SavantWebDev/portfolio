fe7bdd3f3c00645e29fc407d39b22541d3583378
Hudson <hudsonvini26@gmail.com>
2024-03-21 13:16:52 -0300

    Add new area on ColaboratorsArea and Transform elements in Strip component

--- src/app/components/home/ColaboratorsArea/ColaboratorsArea.jsx
+++ src/app/components/home/ColaboratorsArea/ColaboratorsArea.jsx
@@ -7,15 +7,33 @@ import Strip from '../Strip/Strip'
 export default function ColaboratorsArea() {
     return (
         <section className={style.container}>
+            {/* Strip */}
             <Strip />
-
+            {/* Container Cards */}
             <div className={style.containerCards}>
 
-                <CardsColaborators />
-                
-                <CardsColaborators />
-                
-                <CardsColaborators />
+                {/* Slider Cards */}
+                <div className={style.sliderCards}>
+                    <CardsColaborators />
+                    
+                    <CardsColaborators />
+                    
+                    <CardsColaborators />
+                </div>
+            </div>
+
+            {/* Strip */}
+            <Strip order />
+            {/* Container Cards */}
+            <div className={`${style.containerCards} ${style.containerDesign}`}>
+
+                {/* Slider Cards */}
+                <div className={style.sliderCards}>
+                    <CardsColaborators />
+                    
+                    <CardsColaborators />
+                    
+                </div>
             </div>
 
         </section>
